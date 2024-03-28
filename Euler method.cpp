6fdc6d5a057c5570ac0026700efc87678056939a
avocadosamsam <kimwhayoung1717@gmail.com>
2024-03-28 23:09:02 +0900

    start Euler Method

---
+++ Euler method.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+double dphidt(double t, double phi_t) {
+	return 3.*sin(6.0*t);
+}
+
+int main(){
+    int nStep, nPrecision=5, nWidth=15;
+    
+
+
+
+}
